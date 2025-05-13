b4b4506a79cc3091c18333753671902804d2dae8
Nguyễn Khắc Đạt <162654427+NguyenKhacDatt@users.noreply.github.com>
2025-05-13 22:47:45 +0700

    Add files via upload

---
+++ LAB 1.5/Bai2/Hinh.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+public abstract class Hinh
+{
+    public abstract double TinhChuVi();
+    public abstract double TinhDienTich();
+}
+
+public class HinhTron : Hinh
+{
+    private double banKinh;
+
+    public HinhTron(double banKinh)
+    {
+        this.banKinh = banKinh;
+    }
+
+    public override double TinhChuVi()
+    {
+        return 2 * Math.PI * banKinh;
+    }
+
+    public override double TinhDienTich()
+    {
+        return Math.PI * banKinh * banKinh;
+    }
+
+    public override string ToString()
+    {
+        return $"Hinh Tron: Chu vi = {TinhChuVi():F2}, Dien tich = {TinhDienTich():F2}";
+    }
+}
